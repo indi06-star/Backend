313d91af401c0a2964e91b9c4796a3790832fe6f
chantewyngaardt <chantexwyngaardt@icloud.com>
2025-06-17 12:33:06 +0200

    change user to admin

--- index.php
+++ index.php
@@ -26,14 +26,14 @@ echo json_encode([
     "message" => "Welcome to the Admin Management PHP Backend API",
     "status" => "OK",
     "available_routes" => [
-        "POST /user.php?action=signup" => "Create a new admin (requires main admin password)",
-        "GET /user.php?action=getAll" => "Fetch all admins",
-        "GET /user.php?action=get&username={username}" => "Fetch a specific admin by username",
-        "GET /user.php?action=get&id={id}" => "Fetch a specific admin by ID",
-        "PUT /user.php?action=update&username={username}" => "Update admin details by username",
-        "PUT /user.php?action=update&id={id}" => "Update admin details by ID",
-        "DELETE /user.php?action=delete&id={id}" => "Delete an admin by ID",
-        "DELETE /user.php?action=delete&username={username}" => "Delete an admin by username",
+        "POST /admin.php?action=signup" => "Create a new admin (requires main admin password)",
+        "GET /admin.php?action=getAll" => "Fetch all admins",
+        "GET /admin.php?action=get&username={username}" => "Fetch a specific admin by username",
+        "GET /admin.php?action=get&id={id}" => "Fetch a specific admin by ID",
+        "PUT /admin.php?action=update&username={username}" => "Update admin details by username",
+        "PUT /admin.php?action=update&id={id}" => "Update admin details by ID",
+        "DELETE /admin.php?action=delete&id={id}" => "Delete an admin by ID",
+        "DELETE /admin.php?action=delete&username={username}" => "Delete an admin by username",
         "POST/login.php?action=login" => "Admin Login"
     ]
 ]);
